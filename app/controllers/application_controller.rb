37b18a0fa74838aa40f5feb86047fb817eb4e2e1
Koichi-Nak <koichi.nakayama1227@gmail.com>
2020-10-16 04:03:29 +0900

    application controllerの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name_kan, :last_name_kan, :family_name_kana, :last_name_kana, :birth_year, :birth_month, :birth_day])
+  end
+
 end
