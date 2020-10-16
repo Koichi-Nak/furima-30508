64ec485a2a43dded61e383c13ad67c439d9c88ce
Koichi-Nak <koichi.nakayama1227@gmail.com>
2020-10-16 11:08:01 +0900

    dateカラムの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :first_name_kan,  presence: true
+  validates :email,            presence: true
+  validates :first_name_kan,   presence: true
   validates :last_name_kan,    presence: true
-  validates :first_name_kana, presence: true
+  validates :first_name_kana,  presence: true
   validates :last_name_kana,   presence: true
   validates :birth_date,       presence: true
 
