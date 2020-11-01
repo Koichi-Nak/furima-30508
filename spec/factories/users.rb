6b6b9277f3c1518be57c87018d19c40329a49552
Koichi-Nak <koichi.nakayama1227@gmail.com>
2020-11-02 07:20:47 +0900

    Fakerの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     nickname = Faker::Name.initials(number: 2)
     nickname {nickname}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6, mix_case: true)
+    password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
     last_name_kan {Gimei.last.kanji}
