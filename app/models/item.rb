44cb7469334596586089630ac28c72f19c851315
Koichi-Nak <koichi.nakayama1227@gmail.com>
2020-11-05 17:40:54 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,26 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :delivery_charge
+  belongs_to :shipping_from
+  belongs_to :shipping_preparation
 
   with_options presence: true do
     validates :item_name
     validates :description
+    validates :price, format: { :greater_than_or_equal_to => 300,
+                                :less_than_or_equal_to => 9999999,
+                                :message => 'Out of setting range' }
+    #validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  end
+
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :item_status_id
     validates :delivery_charge_id
     validates :shipping_from_id
     validates :shipping_preparation_id
-    validates :price, format: { :greater_than_or_equal_to => 300,
-                                :less_than_or_equal_to => 9999999,
-                                :message => 'Out of setting range' }
-    #validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
   end
 
   belongs_to :user
---
+++ spec/factories/shipping_preparations.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :shipping_preparation do
+    
+  end
+end
