3e300545604feff2e31312dea7637ad123e6733c
Koichi-Nak <koichi.nakayama1227@gmail.com>
2020-11-06 10:36:55 +0900

    createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,21 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :item_status_id, :delivery_charge_id, :shipping_from_id, :shipping_preparation_id, :price).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,9 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :price, format: { :greater_than_or_equal_to => 300,
-                                :less_than_or_equal_to => 9999999,
-                                :message => 'Out of setting range' }
+    validates :price, numericality: { :greater_than_or_equal_to => 300,
+                                      :less_than_or_equal_to => 9999999,
+                                      :message => 'Out of setting range' }
     #validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
   end
 
