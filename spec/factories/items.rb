ba9e25313775fbe96997343788ed14f672624e9f
Koichi-Nak <koichi.nakayama1227@gmail.com>
2020-11-09 11:46:07 +0900

    FactoryBotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :item do
-    item_name { Faker::Name.name(max_length: 40) }
-    description { Faker::Text.characters(max_length: 1000) }
+    item_name { Faker::Lorem.word }
+    description { Faker::Lorem.characters(number: 1..1000) }
     category_id { Faker::Number.between(from: 2, to: 11) }
     item_status_id { Faker::Number.between(from: 2, to: 7) }
     delivery_charge_id { Faker::Number.between(from: 2, to: 3) }
     shipping_from_id { Faker::Number.between(from: 2, to: 48) }
     shipping_preparation_id { Faker::Number.between(from: 2, to: 4) }
     price { Faker::Number.between(from: 300, to: 9999999) }
+    user
 
     after(:build) do |pic|
       pic.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
